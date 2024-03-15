596d0457eadaec96282ab1badb6f1fba3ea7ef7e
Warren Rodie <warren@rodie.net>
2024-03-15 14:37:55 +1100

    bug: fix startup issues (#40)
    * bug: fix startup issues
    
    Better handle errors when plugin is starting and the mixer is not available.
    Update some text
    require new version of behringer_mixer.py
    
    * bump version

--- custom_components/ha_behringer_mixer/const.py
+++ custom_components/ha_behringer_mixer/const.py
@@ -6,4 +6,4 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "Behringer Mixer"
 DOMAIN = "ha_behringer_mixer"
 VERSION = "0.1.1"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+ATTRIBUTION = ""
